673a3ffcfb338d8784c35268a1b3fd989a159951
ducvu <vuduc2701@gmail.com>
2021-12-21 13:30:20 +0700

    Create_GetData_VVD483

---
+++ LTQL_1721050483/LTQL_1721050483/Controllers/View2bangsController.cs
@@ -0,0 +1,23 @@
+﻿using LTQL_1721050483.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTQL_1721050483.Controllers
+{
+    public class View2bangsController : Controller
+    {
+        LTQLDb db = new LTQLDb();
+        // GET: View2bangs
+        public ActionResult Index()
+        {
+            var viewModel =
+      from pd in db.NhaCungCaps
+      join p in db.SanPhams on pd.Manhacungcap equals p.Manhacungcap
+      select new View2bang { nhaCungCap = pd, SanPham = p };
+            return View(viewModel);
+        }
+    }
+}
---
+++ LTQL_1721050483/LTQL_1721050483/Models/View2bang.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050483.Models
+{
+    public class View2bang
+    {
+        public SanPham SanPham { get; set; }
+        public NhaCungCap nhaCungCap { get; set; }
+    }
+}
